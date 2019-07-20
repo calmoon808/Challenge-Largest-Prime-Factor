4bcc9e8a43d1911bfb470c27db29481b645a9444
Calvin <calmoon808@gmail.com>
2019-07-20 11:16:05 -1000

    large primes

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,13 @@
 exports.largestPrimeFactor = function(n) {
   let primeNumber = 0;
   // do your work here
-
-  return primeNumber;
+  let i = 3;
+  while (i <= n){
+    if (n % i === 0){
+      n /= i;
+    } else {
+      i += 2;
+    }
+  }
+  return i;
 };
